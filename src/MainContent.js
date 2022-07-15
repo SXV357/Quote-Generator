253aeb5a807d47b8d06ff1d0ec2c2aafb3f46e53
SXV357 <64874986+SXV357@users.noreply.github.com>
2022-07-15 22:21:54 +0530

    Data file and quote state modifications

--- src/MainContent.js
+++ src/MainContent.js
@@ -3,17 +3,17 @@ import QuotesData from "./QuotesData"
 
 export default function MainContent(){
 
-    const [quotes, setQuotes] = React.useState();
+    const [quotes, setQuotes] = React.useState('');
 
     function getQuote(){
-      let quoteArray = QuotesData.quotes;
-      let index = Math.floor(Math.random() * quoteArray.length);
-      setQuotes(quoteArray[index].quote);
+      let array = QuotesData  
+      let index = Math.floor(Math.random() * array.length);
+      return setQuotes(array[index].quote + ' - ' + array[index].name);
     }
     return(
         <div className = "quote-container">
             <div className = "quote-box">
-                <p className = "quote">❝{quotes}❞</p>
+                <p className = "quote">{quotes}</p>
             </div>
             <button className = "quote-generator-button" onClick = {getQuote}>Generate a Quote</button>
         </div>
