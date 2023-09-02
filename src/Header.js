494657a65d649ad948bf8973b3f6b4f055500dec
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-09-02 01:03:30 -0400

    added a print statement to header.js

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 export default function Header(){
+    console.log("Hello World");
     return(
         <div className = "header-container">
             <img className = "header-image" src = "https://th.bing.com/th/id/R.d9a56eb09892cbf6ec98b6f1b4d94dd6?rik=s3tHM8YMNiceWw&pid=ImgRaw&r=0" alt = "Sunset"/>
